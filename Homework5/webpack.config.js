40f907ee0d30103a6882cc7b48e8e4c96b1e086c
Plyton <emenevr@mail.ru>
2021-05-18 00:17:48 +0400

    Homework05

---
+++ Homework5/webpack.config.js
@@ -0,0 +1,23 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader'],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].css',
+            chunkFilename: 'based on filename'
+        }),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        })
+    ],
+}
